499bf3fd0f3228b468036ef62d58955aeaca6ff6
Christian Santos <stchrist14@gmail.com>
2024-06-05 21:46:05 -0300

    Jogo Do Número Secreto

---
+++ app.js
@@ -0,0 +1,50 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+function mensagemInical(){
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+}
+mensagemInical();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if(chute == numeroSecreto){
+        let palavraTentativa = tentativas > 1 ? 'Tentativas' : 'Tentativa';
+        let mensagemTentativa = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('h1', 'Parabéns, você acertou');
+        exibirTextoNaTela('p', mensagemTentativa);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else{
+        if(chute > numeroSecreto){
+            exibirTextoNaTela('p','O numero secreto é menor!');
+        }else{
+            exibirTextoNaTela('p','O número secreto é maior!');
+        }
+    }
+    tentativas++;
+    limparCampo()
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = ''
+}
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio;
+    limparCampo();
+    tentativas = 1;
+    mensagemInical();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
